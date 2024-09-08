b7ee42df546ed3f491b028dfb3f9770d944da802
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-09-08 13:30:14 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,16 @@ export default function App() {
     <div>
       <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,9 @@
-export default function Product() {
+export default function Product({ name, imgUrl, price }) {
   return (
     <div>
-      <h2>Tacos</h2>
-	  <p>Price: 999 credits</p>
+      <h2>{name}</h2>
+	  <img src={imgUrl} alt={name} width="480" />
+	  <p>Price: {price} credits</p>
     </div>
   );
 };
